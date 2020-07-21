4563331df712f51047740c6ca3a2f5f278fbbe87
getimiskon <getimiskon@gmail.com>
2020-07-22 00:23:45 +0300

    Various fixes and additions

--- bot-src.py
+++ bot-src.py
@@ -1,57 +1,87 @@
 import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
 class MyClient(discord.Client):
-
-    async def on_ready(self):
-
-        print("")
-        print("Logged in as:")
-        print(self.user.name)
-        print(self.user.id)
-        print("------")
-
-    async def on_message(self, message):
-
-        if message.author.id == self.user.id:
-            return
-
-        if message.content.find("Hello Lain") != -1:
-            await message.channel.send("Hello {0.author.mention}!".format(message))
-
-        elif message.content.find("こんにちは玲音") != -1:
-            await message.channel.send("こんにちは{0.author.mention}さん！".format(message))
-
-        elif message.content.find("play track 44") != -1:
-            await message.channel.send("https://www.youtube.com/watch?v=w_wx2jag7xI")
-
-        elif message.content.find("What is memory?") != -1:
-            await message.channel.send("A memory is only a record. You just have to rewrite that record.")
-
-        elif message.content.find("Hi Lain") != -1:
-            await message.channel.send("https://raw.githubusercontent.com/getimiskon/lainbot/master/assets/lain-wave.png")
-        
-        elif message.content.find("Present day!") != -1:
-            await message.channel.send("Present time!")
-            await message.channel.send("HAHAHAHAHAHA!")
-
-        elif message.content.find("プレゼント・デイ") != -1:
-            await message.channel.send("プレゼント・タイム")
-            await message.channel.send("はははははは！")
-
-        elif message.content.find("How are you, Lain?") != -1:
-            await message.channel.send("I'm fine. How about you?")
-
-        elif message.content.find("I'm fine.") != -1:
-            await message.channel.send("That's good")
+	
+	# Command prefix
+	bot = commands.Bot(command_prefix='>')
+	
+	# Bot's name and ID, can be viewed in a command line interface
+	async def on_ready(self):
+		print("")
+		print("Logged in as:")
+		print(self.user.name)
+		print(self.user.id)
+		print("------")
 
 
-    async def on_member_join(self, member):
-        guild = member.guild
-        if guild.system_channel is not None:
-            to_send = "Welcome to the {0.name}, {1.mention}.".format(guild, member)
-            await guild.system_channel.send(to_send)
+	# Various messages
+	async def on_message(self, message):
+		
+		if message.author.id == self.user.id:
+			return
+		
+		# Hello Lain 1 - English	
+		if message.content.find("Hello Lain") != -1:
+			await message.channel.send("Hello {0.author.mention}!".format(message))
+		
+		# Hello Lain 2 - English
+		if message.content.find("Hi Lain") != -1:
+			await message.channel.send("https://raw.githubusercontent.com/getimiskon/lainbot/master/assets/lain-wave.png")			
+		
+		# Hello Lain 3 - Japanese	
+		if message.content.find("こんにちは玲音") != -1:
+			await message.channel.send("こんにちは{0.author.mention}さん！".format(message))
+			
+		if message.content.find("Ping") != -1:
+			await message.channel.send("Pong!")
+		
+		# Various references to Serial Experiments Lain	
+		if message.content.find("play track 44") != -1:
+			await message.channel.send("https://www.youtube.com/watch?v=w_wx2jag7xI")
+			
+		if message.content.find("What is memory?") != -1:
+			await message.channel.send("A memory is only a record. You just have to rewrite that record.")
+						
+		if message.content.find("Present day!") != -1:
+			await message.channel.send("Present time!")
+			await message.channel.send("HAHAHAHAHAHA!")
+		
+		if message.content.find("プレゼント・デイ") != -1:
+			await message.channel.send("プレゼント・タイム")
+			await message.channel.send("はははははは！")
+		
+		# "How are you?" question	
+		if message.content.find("How are you, Lain?") != -1:
+			await message.channel.send("I'm fine. How about you?")
+		
+		# "I'm fine" reply - to be removed
+		if message.content.find("I'm fine") != -1:
+			await message.channel.send("That's good")
+		
+		# help(?) command	
+		if message.content.find("Help me Lain") != -1:
+			await message.channel.send("For any help, contact @getimiskon#7962.")
+		
+		# reference to "nobody here video" from YouTube	
+		if message.content.find("anybody here") != -1:
+			await message.channel.send("https://www.youtube.com/watch?v=-RFunvF0mDw")
+			
+		#Swear words
+		if message.content.find("fuck") != -1:
+			await message.channel.send("Calm down, {0.author.mention}.".format(message))
 
+	# Welcome message for new members
+	async def on_member_join(self, member):
+		guild = member.guild
+		if guild.system_channel is not None:
+			to_send = "Welcome to the {0.name}, {1.mention}.".format(guild, member)
+			await guild.system_channel.send(to_send)
+			
+# Client-specific data
 client = MyClient()
-client.run("")
-
-# The client.run string has the token from Discord. It won't be uploaded here. 
+TOKEN = os.getenv("DISCORD_TOKEN")
+client.run(TOKEN)
