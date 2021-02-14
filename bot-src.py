13029030900a5ce84d22a4b0e46e1bd02a23aabd
getimiskon <getimiskon@protonmail.com>
2021-02-14 22:09:03 +0200

    Updated the source code

--- bot-src.py
+++ bot-src.py
@@ -4,12 +4,12 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 
-class MyClient(discord.Client):
+class MainClient(discord.Client):
 	
-	# Command prefix
-	bot = commands.Bot(command_prefix='>')
+# Command prefix (DOESN'T WORK - MUST BE FIXED)
+#	bot = commands.Bot(command_prefix='>')
 	
-	# Bot's name and ID, can be viewed in a command line interface
+# Bot's name and ID, can be viewed in a command line interface
 	async def on_ready(self):
 		print("")
 		print("Logged in as:")
@@ -18,28 +18,29 @@ class MyClient(discord.Client):
 		print("------")
 
 
-	# Various messages
+# Various messages
 	async def on_message(self, message):
 		
 		if message.author.id == self.user.id:
 			return
 		
-		# Hello Lain 1 - English	
+# Hello Lain 1 - English	
 		if message.content.find("Hello Lain") != -1:
 			await message.channel.send("Hello {0.author.mention}!".format(message))
 		
-		# Hello Lain 2 - English
+# Hello Lain 2 - English
 		if message.content.find("Hi Lain") != -1:
 			await message.channel.send("https://raw.githubusercontent.com/getimiskon/lainbot/master/assets/lain-wave.png")			
 		
-		# Hello Lain 3 - Japanese	
+# Hello Lain 3 - Japanese
 		if message.content.find("こんにちは玲音") != -1:
 			await message.channel.send("こんにちは{0.author.mention}さん！".format(message))
-			
+
+# Ping command - useful for troubleshooting purposes			
 		if message.content.find("Ping") != -1:
 			await message.channel.send("Pong!")
 		
-		# Various references to Serial Experiments Lain	
+# Various references to Serial Experiments Lain	
 		if message.content.find("play track 44") != -1:
 			await message.channel.send("https://www.youtube.com/watch?v=w_wx2jag7xI")
 			
@@ -54,34 +55,41 @@ class MyClient(discord.Client):
 			await message.channel.send("プレゼント・タイム")
 			await message.channel.send("はははははは！")
 		
-		# "How are you?" question	
-		if message.content.find("How are you, Lain?") != -1:
-			await message.channel.send("I'm fine. How about you?")
+# "How are you?" question	
+#		if message.content.find("How are you, Lain?") != -1:
+#			await message.channel.send("I'm fine. How about you?")
 		
-		# "I'm fine" reply - to be removed
-		if message.content.find("I'm fine") != -1:
-			await message.channel.send("That's good")
+# "I'm fine" reply - to be removed
+#		if message.content.find("I'm fine") != -1:
+#			await message.channel.send("That's good")
 		
-		# help(?) command	
-		if message.content.find("Help me Lain") != -1:
-			await message.channel.send("For any help, contact @getimiskon#7962.")
+# Help command	
+#		if message.content.find("Help me Lain") != -1:
+#			await message.channel.send("For any help, contact @getimiskon#7962.")
 		
-		# reference to "nobody here video" from YouTube	
+# reference to "nobody here video" from YouTube	
 		if message.content.find("anybody here") != -1:
 			await message.channel.send("https://www.youtube.com/watch?v=-RFunvF0mDw")
 			
-		#Swear words
-		if message.content.find("fuck") != -1:
-			await message.channel.send("Calm down, {0.author.mention}.".format(message))
+# Swear words
+#		if message.content.find("fuck") != -1:
+#			await message.channel.send("Calm down, {0.author.mention}.".format(message))
 
-	# Welcome message for new members
-	async def on_member_join(self, member):
-		guild = member.guild
-		if guild.system_channel is not None:
-			to_send = "Welcome to the {0.name}, {1.mention}.".format(guild, member)
-			await guild.system_channel.send(to_send)
+# "Dead chat" reply
+		if message.content.find("dead chat") != -1:
+			await message.channel.send("https://discord.com/channels/270375970706423808/796889008524230698/808370338923544587")
+			
+		if message.content.find("dead chat xD") != -1:
+			await message.channel.send("https://discord.com/channels/270375970706423808/796889008524230698/808370338923544587")
+
+#Welcome message for new members (BROKEN - MUST BE FIXED)
+#	async def on_member_join(self, member):
+#		guild = member.guild
+#		if guild.system_channel is not None:
+#			to_send = "Welcome to the {0.name}, {1.mention}.".format(guild, member)
+#			await guild.system_channel.send(to_send)
 			
 # Client-specific data
-client = MyClient()
+client = MainClient()
 TOKEN = os.getenv("DISCORD_TOKEN")
 client.run(TOKEN)
